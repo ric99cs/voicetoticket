8cfa279090f63bd3717e9fcb6d93c238df333e11
Richie <43894274+ric99cs@users.noreply.github.com>
2022-01-20 17:40:31 +0100

    Create voicetoticket.php

---
+++ voicetoticket.php
@@ -0,0 +1,45 @@
+#!/usr/bin/php -q
+<?php
+
+require('config.php');
+include('functions.php');
+
+$file = $argv[1];
+$cid = $argv[2];
+
+$url = Microsoft_Endpoint."?language=de-DE";
+$result = CallAPI('POST',$url,Microsoft_Headers,file_get_contents($file));
+$jdecode = json_decode($result);
+
+if ($jdecode->RecognitionStatus == 'Success') {
+    $htmlContent = "Es gibt eine neue Nachricht von der Rufnummer " . $cid . "<br />Folgender Text wurde erkannt:<br><br><strong>" . $jdecode->DisplayText . "</strong><br /><br />Die Nachricht ist zum Nachhören als Anhang beigefügt.";
+} else {
+    $htmlContent = "Es gibt eine neue Nachricht von der Rufnummer " . $cid . "<br />Die Nachricht konnte nicht erkannt werden!<br /><br />Die Nachricht ist zum Nachhören als Anhang beigefügt.";
+}
+
+$userid = GetUser($cid);
+$jdecode = json_decode($result);
+$data = GetBase64($file);
+$list[] = array('filename' => basename($file), 'data' => $data, 'mime-type' => 'audio/wav');
+
+if ($userid == 0) {
+    $userid = NewUser($cid);
+}
+
+$postdata = array(
+    "title" => "Neue Nachricht auf dem Anrufbeantworter",
+    "customer_id" => $userid,
+    "group_id" => 14,
+    "article" => array(
+        "subject" => "Neue Nachricht",
+        "body" => $htmlContent,
+        "content_type" => "text/html",
+        "type" => "note",
+        "internal" => false,
+        "attachments" => $list
+    )
+);
+$jsondata = json_encode($postdata);
+$final = CallAPI('POST', Zammad_Endpoint.'/tickets',Zammad_Headers, $jsondata);
+
+?>
