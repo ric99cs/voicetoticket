ea792253193f1ca95e0cddb3fba300119e8a9a21
Richie <43894274+ric99cs@users.noreply.github.com>
2022-01-25 15:38:42 +0100

    Update voicetomail.php

--- voicetomail.php
+++ voicetomail.php
@@ -18,29 +18,6 @@ if ($jdecode->RecognitionStatus == 'Success') {
     $htmlContent = "There is a new voicemail from the caller " . $cid . "<br />The message text could not be detected!<br /><br />The voicemail is attached as audio file.";
 }
 
-$userid = GetUser($cid);
-$jdecode = json_decode($result);
-$data = GetBase64($file);
-$list[] = array('filename' => basename($file), 'data' => $data, 'mime-type' => 'audio/wav');
-
-if ($userid == 0) {
-    $userid = NewUser($cid);
-}
-
-$postdata = array(
-    "title" => "Neue Nachricht auf dem Anrufbeantworter",
-    "customer_id" => $userid,
-    "group_id" => 14,
-    "article" => array(
-        "subject" => "Neue Nachricht",
-        "body" => $htmlContent,
-        "content_type" => "text/html",
-        "type" => "note",
-        "internal" => false,
-        "attachments" => $list
-    )
-);
-$jsondata = json_encode($postdata);
-$final = CallAPI('POST', Zammad_Endpoint.'/tickets',Zammad_Headers, $jsondata);
+sendMailWithAttachment($recipient, $file, 'New Message at your mailbox', $htmlContent);
 
 ?>
